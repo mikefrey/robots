c26b6761733d4cdb2036dafdaeaf35fbd76f1bef
Mike Frey <mike.frey@tstmedia.com>
2013-05-11 18:04:12 -0500

    made switch work

--- js/switch.js
+++ js/switch.js
@@ -1,5 +1,22 @@
 function Switch(pos) {
   this.pos = pos
+  this.state = Switch.STATE.OFF
+}
+
+Switch.prototype.turnOn = function(ent) {
+  if (this.state === Switch.STATE.OFF) {
+    this.state = Switch.STATE.ON
+    return true
+  }
+  return false
+}
+
+Switch.prototype.turnOff = function(ent) {
+  if (this.state === Switch.STATE.ON) {
+    this.state = Switch.STATE.OFF
+    return true
+  }
+  return false
 }
 
 Switch.prototype.update = function() {
@@ -15,10 +32,23 @@ Switch.prototype.draw = function(ctx) {
 
     var radius = scale*0.3
 
+    // fill the square
+    ctx.fillStyle = this.state === Switch.STATE.ON ? '#00FF00' : '#FF0000'
+    ctx.beginPath()
+    ctx.rect(-scale/2, -scale/2, scale, scale)
+    ctx.fill()
+    ctx.stroke()
+
+    // draw the reciever
     ctx.fillStyle = '#FFFFFF'
     ctx.beginPath()
     ctx.arc(0, 0, radius, d2r(0), d2r(360))
     ctx.fill()
     ctx.stroke()
   }.bind(this))
+}
+
+Switch.STATE = {
+  ON : 1,
+  OFF : 0
 }
